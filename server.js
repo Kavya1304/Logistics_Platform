43d3f58dccce8fd9d11031235743a0dd216032f0
Padala Kavya <kavyapadala890@gmail.com>
2024-11-12 06:35:07 +0530

    removed unused files

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 
 // Middleware to serve static files from the "public" directory
 app.use(express.static('public'));
